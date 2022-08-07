e3fb5919748ac30175738410b3db91dacc292da9
Asynco <101818400+Asyncod@users.noreply.github.com>
2022-08-07 06:20:14 +0300

    нужно это, ну нжнО!

---
+++ bot_xd/config.py
@@ -0,0 +1,25 @@
+captcha_list = {"F3dn8e6" : "AgACAgIAAxkBAAMGYqMcFXdBEgZWuwXRNb3Ank6d81UAAoi5MRvfvRhJydKWDtZw3G8BAAMCAANtAAMkBA",
+                "YOrVVTz" : "AgACAgIAAxkBAAMHYqMcJSMJkiYjo921V-qBOlB_CXwAAom5MRvfvRhJtSM3Sm48W5IBAAMCAANtAAMkBA",
+                "ywlmkaN" : "AgACAgIAAxkBAAMIYqMcOQ9P99wK1YtsUl_gnbHB5hkAAoq5MRvfvRhJYs4Ou612GYwBAAMCAANtAAMkBA",
+                "1xRb22b" : "AgACAgIAAxkBAAMJYqMcPao3J-7KEJ8rdhLxrOwgKU0AAou5MRvfvRhJ1gSKUskTh2EBAAMCAANtAAMkBA",
+                "7tqr7gG" : "AgACAgIAAxkBAAMKYqMcRhX3j_e-zckXPGpGLbN7mIkAAoy5MRvfvRhJIsm6lq-_ObgBAAMCAANtAAMkBA",
+                "KV5IU04" : "AgACAgIAAxkBAAMLYqMcaP3Ml7OFewOk-6_nQNc030YAAo25MRvfvRhJ2l2MVtUDTa0BAAMCAANtAAMkBA",
+                "IYaaAr3" : "AgACAgIAAxkBAAMMYqMcdpZCfl5Mh7ZKXrZU1rIZ9uQAAo65MRvfvRhJJ5bDmePgFEQBAAMCAANtAAMkBA",
+                "Y4zObpH" : "AgACAgIAAxkBAAMNYqMchsxzkNaZFkEnDESISPzJ6mwAAo-5MRvfvRhJ248dlKiBxDgBAAMCAANtAAMkBA",
+                "jNV9JyC" : "AgACAgIAAxkBAAMOYqMclr5hez83ZXU4eQWva8rPDckAApC5MRvfvRhJfnx7KyHeTSUBAAMCAANtAAMkBA",
+                "iQpFEqT" : "AgACAgIAAxkBAAMPYqMcoyOzoPr5-rm6HCfDSqLR0h0AApK5MRvfvRhJnw7h_KEjcrYBAAMCAANtAAMkBA",
+                "RJUo5VU" : "AgACAgIAAxkBAAMQYqMcsOnfzKtME3IixQ2Suj1aaJcAApO5MRvfvRhJtSubfooVlTsBAAMCAANtAAMkBA",
+                "hITUgS8" : "AgACAgIAAxkBAAMRYqMcvNSsXiA6o4Rcu7liCT80v6AAApS5MRvfvRhJr03THWgOtzEBAAMCAANtAAMkBA",
+                "DYNPTIy" : "AgACAgIAAxkBAAMSYqMcyyB4zAAB404eDPvQm6jTWgiyAAKVuTEb370YSZvj2-nROThBAQADAgADbQADJAQ",
+                "ZAOJciT" : "AgACAgIAAxkBAAMTYqMc1_ByZL4VP2kLjxzYNtP9RDwAApa5MRvfvRhJAAHYNDtU6wjyAQADAgADbQADJAQ",
+                "vt3psxq" : "AgACAgIAAxkBAAMUYqMc4ft3UiZBzLfWqyWRVC-omKMAApe5MRvfvRhJhi9MUq7uPkgBAAMCAANtAAMkBA",
+                "48EWeR7" : "AgACAgIAAxkBAAMVYqMc7cvUW2B63pXXYveZpNIM6sUAApi5MRvfvRhJ-MXJchPiZm8BAAMCAANtAAMkBA",
+                "vklZtgx" : "AgACAgIAAxkBAAMWYqMc-L-0ZqtL4KJnnGaK6vZDxQ4AApq5MRvfvRhJKPrAaOlON5kBAAMCAANtAAMkBA",
+                "kPZKxVZ" : "AgACAgIAAxkBAAMXYqMdAjw65iV2Vy0hhuPjoGic_3UAApu5MRvfvRhJUXewQM1JXvoBAAMCAANtAAMkBA",
+                "7ruudwP" : "AgACAgIAAxkBAAMYYqMdC5FjX3YHsnH3_B1bSXeS6cYAApy5MRvfvRhJfUeEgp1JllIBAAMCAANtAAMkBA"} # Это короче список ID фоток каптчи, разберетесь
+
+bot_token = "5456415167:123123123123123213123" # Токен бота
+channel_referal = "bot_долбаеба" # Название бота для реферальной ссылки
+appl_token = "46890:1231231232132131231231" # Из криптобота токен приложения
+chat_id = "-" # ID чата канала
+channel_id = "@" # ID канала
